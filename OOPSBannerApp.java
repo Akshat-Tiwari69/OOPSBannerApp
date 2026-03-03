1a14a49059af2251fbc570902db5f7e807e45ad4
Akshat Tiwari <tiwaribabudeveloper@gmail.com>
2026-03-03 08:25:20 +0530

    UC1: Simple output - Print OOPS to console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,18 @@
+/**
+ * OOPSBannerApp UC1 - Banner Display Application
+ *
+ * This class demonstrates the first user story:
+ *
+ * UC1: Simple output
+ * - Prints the string "OOPS" to the console using a single print statement.
+ *
+ * @author Akshat Tiwari
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        // UC1: simple output
+        // Print the literal message as in the first user story.
+        System.out.println("OOPS");
+    }
+}
